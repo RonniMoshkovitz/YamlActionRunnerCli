16910ad8b59841c7700476887b28fdae664add4b
RonniMoshkovitz <117098162+RonniMoshkovitz@users.noreply.github.com>
2025-10-19 16:47:58 +0300

    Added runner and steps processing

---
+++ YamlActionRunnerCli/InstructionsRunning/InstructionsRunner.cs
@@ -0,0 +1,21 @@
+﻿using YamlActionRunnerCli.ActionManagement;
+using YamlActionRunnerCli.Utils.DataObjects.Instructions;
+using YamlActionRunnerCli.Utils.DataObjects.Run;
+
+namespace YamlActionRunnerCli.InstructionsRunning;
+
+public class InstructionsRunner(Scope scope)
+{
+    private Scope _scope = scope;
+    private StepsProcessor _stepsProcessor = new(scope);
+
+
+    //handle error code?
+    public void Run(Instructions instructions)
+    {
+        foreach (var step in _stepsProcessor.ProcessSteps(instructions.Steps))
+        {
+            step.Run(_scope);
+        }
+    }
+}
---
+++ YamlActionRunnerCli/InstructionsRunning/StepsProcessor.cs
@@ -0,0 +1,36 @@
+﻿using YamlActionRunnerCli.ActionManagement;
+using YamlActionRunnerCli.ActionManagement.Actions;
+using YamlActionRunnerCli.InstructionsRunning.ScopeApplying;
+using YamlActionRunnerCli.Utils.DataObjects.Instructions;
+using YamlActionRunnerCli.Utils.DataObjects.Run;
+
+namespace YamlActionRunnerCli.InstructionsRunning;
+
+public class StepsProcessor(Scope scope)
+{
+    private const string VARIABLES_PATTERN = @"\$\{([^}]+)\}";
+    
+    private VariablesManager _variablesManager = new(VARIABLES_PATTERN);
+    private ActionFactory _actionFactory = new();
+    private Scope _scope = scope;
+
+    public IEnumerable<IAction> ProcessSteps(IEnumerable<Step> steps)
+    {
+        foreach (var step in steps)
+        {
+            ApplyScopeToStep(step);
+            yield return _actionFactory.GetAction(step);
+        }
+    }
+
+    private void ApplyScopeToStep(Step step)
+    {
+        foreach (var (key, value) in step.Parameters!)
+        {
+            if (value is string stringValue)
+            {
+                step.Parameters[key] = _variablesManager.PlaceVariablesInData(stringValue, _scope.Variables!);
+            }
+        }
+    }
+}
