c54d912c415e56a65270baa5d0d8606434b61156
RonniMoshkovitz <117098162+RonniMoshkovitz@users.noreply.github.com>
2025-11-01 20:33:20 +0200

    Added Run CLI command

---
+++ YamlActionRunnerCli/CommandsRunners/BaseCommandRunner.cs
@@ -0,0 +1,11 @@
+﻿using Serilog;
+using YamlActionRunnerCli.Cli.Commands;
+using YamlActionRunnerCli.Utils.DataObjects.Run;
+using YamlActionRunnerCli.Utils.Logging;
+
+namespace YamlActionRunnerCli.CommandsRunners;
+
+public abstract class BaseCommandRunner<TCommand> : ICommandRunner<TCommand> where TCommand : ICommand
+{
+    public abstract int Run(TCommand command);
+}
